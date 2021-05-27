170e472aeeac147e4474bd7f4d3e5c5f7346113e
Kenichi Ooi <kenoi@jp.ibm.com>
2021-05-27 15:08:45 +0900

    create dirctory to do Docker build for tWAS

--- setup-websphere-traditional.sh
+++ setup-websphere-traditional.sh
@@ -12,17 +12,20 @@ $HOME/kubeadmin
 oc registry login
 docker login `oc registry info`
 
-# pull openliberty docker repository
+# pull websphere-traditional docker repository
+# https://github.com/WASdev/ci.docker.websphere-traditional
 WAS_VERSION=9.0.5.7
 docker pull ibmcom/websphere-traditional:${WAS_VERSION}
 
-docker tag websphere-traditional:${WAS_VERSION} $(oc registry info)/$(oc project -q)/websphere-traditional:${WAS_VERSION}
+cd websphere-traditional
 
-docker push $(oc registry info)/$(oc project -q)/websphere-traditional:${WAS_VERSION}
+#docker tag ibmcom/websphere-traditional:${WAS_VERSION} $(oc registry info)/$(oc project -q)/websphere-traditional:${WAS_VERSION}
 
-sed -i s/image-registry.openshift-image-registry.svc:5000/default-route-openshift-image-registry.apps-crc.testing/g twas-kubernetes.yaml
-sed -i s/"\[project-name\]"/$(oc project -q)/g twas-kubernetes.yaml
-sed -i s/"\[was-version\]"/${WAS_VERSION}/g twas-kubernetes.yaml
+#docker push $(oc registry info)/$(oc project -q)/websphere-traditional:${WAS_VERSION}
+
+sed -i s/image-registry.openshift-image-registry.svc:5000/default-route-openshift-image-registry.apps-crc.testing/g kubernetes.yaml
+sed -i s/"\[project-name\]"/$(oc project -q)/g kubernetes.yaml
+sed -i s/"\[was-version\]"/${WAS_VERSION}/g kubernetes.yaml
 
 oc create secret generic docker-user-secret --from-file=.dockerconfigjson=$HOME/.docker/config.json --type=kubernetes.io/dockerconfigjson
 oc apply -f twas-kubernetes.yaml
