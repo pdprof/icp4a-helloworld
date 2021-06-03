64fc530a01539fc91a8b2bd9ff22f288b95a520b
Kenichi Ooi <kenoi@jp.ibm.com>
2021-06-03 12:52:47 +0900

    fix nginx proxy route problem

--- setup-nginx.sh
+++ setup-nginx.sh
@@ -14,7 +14,7 @@ if [ $flag -lt 1 ]; then
 fi
 
 # Copy reverse proxy setting to host
-sed -i s/\[crc-ip\]/$(crc ip)/g host-nginx/server.conf
+sed -i s/"\[crc-ip\]"/$(crc ip)/g host-nginx/server.conf
 sudo cp host-nginx/server.conf /etc/nginx/conf.d/
 
 # Enable and start nginx 
